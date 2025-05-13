ea4a2bbdee2e99b437fd155445afac6b164ab1d6
Roger Wu <weayzehoa@gmail.com>
2025-05-13 15:45:02 +0800

    頁碼設定

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
 use App\Services\GoogleRecaptchaService;
 use Intervention\Image\ImageManager;
@@ -31,6 +32,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Paginator::useBootstrapFour();
         $helperPath = app_path('Helpers/adminHelper.php');
         if (file_exists($helperPath)) {
             require_once $helperPath;
